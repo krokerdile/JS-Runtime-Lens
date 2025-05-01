1b2fe1713cd12cdae5ab7c30f4881c0be6874ce4
krokerdile <crazykroker@gmail.com>
2025-05-02 02:35:15 +0900

    chore: vite.config base 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/JS-Runtime-Lens/', // GitHub repo 이름
   plugins: [react()],
-  base: '/js-runtime-lens/', // GitHub repo 이름
 })
